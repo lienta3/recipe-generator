5e474cf10ae63f3a97991a99fc7061a8a7de4f31
lienta3 <lien.ta3@gmail.com>
2025-11-29 19:57:51 +1100

    added basic js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
+function generateTitles(event) {
+  event.preventDefault();
 
+  let titlesElement = document.querySelector("#titles");
+  titlesElement.innerHTML = "results here";
+}
+
+let titleFormElement = document.querySelector("#title-generator-form");
+titleFormElement.addEventListener("submit", generateTitles);
